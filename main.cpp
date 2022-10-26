ac6d0101b03989bbbd603062b18465629313760d
Sunanda Bose <neel.basu.z@gmail.com>
2022-10-26 15:54:59 +0200

    - project configured with azmq as a submodule

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <azmq/socket.hpp>
+#include <boost/asio.hpp>
+#include <array>
+#include "crn.h"
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
