d3e0bb82d9417fbd94a72a782814f23929a0f119
Sunanda Bose <neel.basu.z@gmail.com>
2022-10-26 15:57:57 +0200

    - azmq basic example compiled

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@
 #include <array>
 #include "crn.h"
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+namespace asio = boost::asio;
+
+int main(int argc, char** argv) {
+    asio::io_service ios;
+    azmq::sub_socket subscriber(ios);
+    subscriber.connect("tcp://192.168.55.112:5556");
+    subscriber.connect("tcp://192.168.55.201:7721");
+    subscriber.set_option(azmq::socket::subscribe("NASDAQ"));
+
+    azmq::pub_socket publisher(ios);
+    publisher.bind("ipc://nasdaq-feed");
+
+    std::array<char, 256> buf;
+    for (;;) {
+        auto size = subscriber.receive(asio::buffer(buf));
+        publisher.send(asio::buffer(buf));
+    }
     return 0;
 }
