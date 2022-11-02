04de3820a6c6c15cd804f420c7e2483b211f4cf2
Neel Basu <neel.basu.z@gmail.com>
2022-11-03 00:54:06 +0100

    - cleaning up

--- main.cpp
+++ main.cpp
@@ -1,23 +1,12 @@
-#include <iostream>
-#include <array>
-#include <string>
-#include <zmq.hpp>
-
-#include "key_pair.h"
-#include "utils.h"
-
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
+#include "db.h"
+#include "server.h"
+#include "keys.h"
 
-#include <cryptopp/integer.h>
-
-#include <nlohmann/json.hpp>
-
-#include <db_cxx.h>
-
-int main(int argc, char** argv) {
-    CryptoPP::AutoSeededRandomPool rng;
-
-    boost::program_options::options_description desc("CLI Frontend for the Trusted Server");
+int main(int argc, char** argv){
+    boost::program_options::options_description desc("CLI Frontend for Data Managers");
     desc.add_options()
         ("help,h", "prints this help message")
         ("public,p", boost::program_options::value<std::string>(), "path to the public key")
@@ -36,66 +25,18 @@ int main(int argc, char** argv) {
     std::string public_key = map["public"].as<std::string>(),
                 secret_key = map["secret"].as<std::string>();
 
-    crn::key_pair keys(public_key, secret_key);
+    crn::db db;
 
-    zmq::context_t context (2);
-    zmq::socket_t socket (context, zmq::socket_type::rep);
-    socket.bind ("tcp://*:5555");
-    while (true) {
-        zmq::message_t request;
+    crn::identity::user master(db, secret_key, public_key);
+    master.init();
 
-        //  Wait for next request from client
-        auto res = socket.recv(request, zmq::recv_flags::none);
-        if(res){
-            nlohmann::json request_json = nlohmann::json::parse(request.str());
-            std::cout << "Received " << request << std::endl;
-            std::string last_str    = request_json["previous"].get<std::string>();
-            CryptoPP::Integer last  = crn::utils::dHex(last_str);
-            CryptoPP::Integer token = crn::utils::dHex(request_json["token"].get<std::string>());
-            Db db(NULL, 0);
-            try{
-                db.open(NULL /* Transaction pointer */,  "blockchain.db", NULL /*Optional logical database name*/ , DB_BTREE, DB_CREATE, 0); // File mode (using defaults)
-                Dbt key((void*) last_str.c_str(), last_str.size()), value;
-                int ret = db.get(NULL, &key, &value, 0);
-                if(ret == DB_NOTFOUND){
-                    std::cout << "not found: " << last_str << std::endl;
-                }else{
-                    // verify
-                    std::string json_str((const char*) value.get_data(), value.get_size());
-                    nlohmann::json json = nlohmann::json::parse(json_str);
-                    CryptoPP::Integer last_checksum = crn::utils::dHex(json["checksum"].get<std::string>());
-                    if( crn::utils::sha512(keys.private_key().raise_x(token)) != last_checksum ){
-                        std::cout << "bad request: " << last_checksum << std::endl;
-                    }else{
-                        // Send Challenge
-                        CryptoPP::Integer active_random = crn::utils::dHex(json["active"][0].get<std::string>());
-                        CryptoPP::Integer rho = keys.random(rng, true), rho_inverse = keys.Gp1().MultiplicativeInverse(rho);
-                        CryptoPP::Integer c1 = keys.Gp().Multiply( token, keys.Gp().Exponentiate(active_random, rho) );
-                        CryptoPP::Integer c2 = keys.Gp().Exponentiate( token, rho_inverse );
-                        CryptoPP::Integer c3 = keys.Gp().Exponentiate(active_random, rho);
+    boost::asio::io_service io;
 
-                        nlohmann::json challenge = {
-                            {"c1", crn::utils::eHex(c1)},
-                            {"c2", crn::utils::eHex(c2)},
-                            {"c3", crn::utils::eHex(c3)}
-                        };
-                        std::string challenge_str = challenge.dump();
-                        // send challenge
-                    }
-                }
-                db.sync(0);
-                db.close(0);
-            }catch(DbException& e){
-                std::cout << e.what() << std::endl;
-            }catch(std::exception& e){
-                std::cout << e.what() << std::endl;
-            }
-        }
+    crn::server server(db, master, io, 9887);
+    server.run();
 
-        //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy(reply.data (), "World", 5);
-        socket.send (reply, zmq::send_flags::none);
-    }
+    io.run();
+
     return 0;
 }
+
